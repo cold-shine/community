a740d069fb305c291ea0c8f285e637e45bb34c8b
cold-shine <lxyleng@gmail.com>
2019-09-06 15:59:33 +0800

    none

--- src/main/java/xin/coldshine/community/controller/AuthorizeController.java
+++ src/main/java/xin/coldshine/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package xin.coldshine.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,15 +16,21 @@ import xin.coldshine.community.provider.GithubProvider;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String clientRedirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("89085e8c8de3ac01c978");
-        accessTokenDTO.setClient_secret("4b536fe5ed4b2609db37b7fe1e1b33a4ce0927b0");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(clientRedirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
